c15deb704c78d258e57a21f7263c20aa1cf3d326
Reed James <reedtjames1234@gmail.com>
2022-01-17 22:27:55 -0600

    change text

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -46,10 +46,10 @@ const Welcome = () => {
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-            Send Crypto <br /> To me!!!!!
+            Web3 Demo <br /> On ETH
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-            Yo, check it out. its gifs on the blockchain
+            Here are the tools used:
           </p>
           {!currentAccount && (
             <button
@@ -65,12 +65,12 @@ const Welcome = () => {
           )}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
-            <div className={commonStyles}>Security</div>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>ReactJS</div>
+            <div className={commonStyles}>TailWind</div>
             <div className={`sm:rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
-            <div className={`sm:rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
-            <div className={commonStyles}>Low Fees</div>
-            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+            <div className={`sm:rounded-bl-2xl ${commonStyles}`}>hardhat</div>
+            <div className={commonStyles}>ethers</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>MetaMask</div>
           </div>
         </div>
 
